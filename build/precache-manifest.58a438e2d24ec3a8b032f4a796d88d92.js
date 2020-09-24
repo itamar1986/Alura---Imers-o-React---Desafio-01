2240549ba7ce7ad3675e777d81e19d647d900932
Itamar1986 <itamar_minnow@hotmail.com>
2020-09-24 15:16:26 -0300

    Verificando bugs

---
+++ build/precache-manifest.58a438e2d24ec3a8b032f4a796d88d92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5832adc4805908c14e4e7bffd88ec1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4093cda8e3ab6eb62f09",
+    "url": "/static/css/main.8477aabe.chunk.css"
+  },
+  {
+    "revision": "10f61b2e830425a10afb",
+    "url": "/static/js/2.df9e76b3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.df9e76b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4093cda8e3ab6eb62f09",
+    "url": "/static/js/main.c2168036.chunk.js"
+  },
+  {
+    "revision": "bd1f42237afcc66da3a5",
+    "url": "/static/js/runtime-main.314cdc6d.js"
+  },
+  {
+    "revision": "606ff8c2a5a79e33664e3771a68458c7",
+    "url": "/static/media/Logo.606ff8c2.png"
+  }
+]);
